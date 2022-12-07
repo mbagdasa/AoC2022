d6157b6035d311e4f87160b89674c537f4cd26ff
Micha <micha@meier-web.ch>
2022-12-07 16:36:38 +0100

    Day07

---
+++ Day07/Program.cs
@@ -0,0 +1,65 @@
+﻿using Input;
+
+namespace Day07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputList = ReadPuzzleInput.GetFullText(7).Split("|")
+                                    .Where(x => !string.IsNullOrEmpty(x))
+                                    .Select(x => x.Split(" ")
+                                    .Where(x => !string.IsNullOrEmpty(x))
+                                    .ToList());
+
+            TreeItem ?currentDirectory = null;
+            foreach (var item in inputList)
+            {
+                if (item[1].Equals("ls"))
+                {
+                    continue;
+                }
+
+                // is command?
+                if (item[0].Equals("$") && item[1].Equals("cd") && item[2].Equals(".."))
+                {
+                    //parent von currentDirectory als currentDirectory setzen
+                    var parentDirectory = currentDirectory.Parent;
+                    currentDirectory = parentDirectory;
+                }
+                else if (item[0].Equals("$") && item[1].Equals("cd"))
+                {
+                    if (currentDirectory is null)
+                    {
+                        currentDirectory = new TreeItem(item[2]);
+                    }
+                    //currentDirectory.Level++;
+                    if (currentDirectory.Children.Count > 0)
+                    {
+                        currentDirectory = currentDirectory.Children.FirstOrDefault(x => x.Name == item[2]);
+                    }
+                }
+                else
+                {
+                    
+                    var child = new TreeItem(item[1])
+                    {
+                        Name = item[1],
+                        Size = item[0].Equals("dir") ? -1 : int.Parse(item[0]),
+                        Parent = currentDirectory,
+                        Level = currentDirectory.Level + 1
+                    };
+
+                    currentDirectory.Children.Add(child);
+                }   
+            }
+
+            while (currentDirectory.Parent is not null)
+            {
+                currentDirectory = currentDirectory.Parent;
+            }
+
+            currentDirectory.printTree();
+        }
+    }
+}
---
+++ Day07/TreeItem.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day07
+{
+    public class TreeItem
+    {
+        public TreeItem ?Parent { get; set; }
+        public List<TreeItem> Children { get; set; }
+        public string Name { get; set; }
+        public int Size { get; set; }
+        public int Level { get; set; }
+
+        public TreeItem(string name)
+        {
+            Name = name;
+            Children = new List<TreeItem>();
+        }
+
+        public void printTree()
+        {
+            Console.WriteLine($"- {String.Concat(Enumerable.Repeat("  ", this.Level))}{this.Name} ({this.Size})");
+
+            foreach (var child in Children)
+            { 
+                child.printTree();
+            }
+        }
+
+        //public void setDirSize()
+        //{
+            
+        //    foreach (var child in Children)
+        //    {
+        //        child.printTree();
+        //    }
+        //}
+
+
+        public TreeItem findChild(string name)
+        {
+            foreach (var child in Children)
+            {
+                if (child.Equals(name))
+                {
+                    return child;
+                }
+                else
+                {
+                    return child.findChild(name);
+                }
+            }
+
+            return null;
+            
+        }
+    }
+}
